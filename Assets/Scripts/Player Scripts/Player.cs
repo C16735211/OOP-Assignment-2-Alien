0ef7b8ba54ab8232c27a8e2e9f3f268590ffb60e
Darren Byrne <c16735211@mydit.ie>
2018-05-02 11:55:01 +0100

    Comments added to Player Script

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    // control maximum velocity of the player
     public float speed = 8f, maxVelocity = 4f;
 
+    // move player and animation on player
     private Rigidbody2D myBody;
     private Animator anim;
 
+    // Awake function we inherit from MonoBehaviour
+    // Apply rigidbody to player prefab
+    // Apply animation to player prefab
     void Awake () {
         myBody = GetComponent<Rigidbody2D> ();
         anim = GetComponent<Animator>();
@@ -19,16 +24,22 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+    // FixedUpdate calls every 3rd or 4th frame
+    // B Function to deal with physics
 	void FixedUpdate () {
         PlayerMoveKeyboard();
 	}
 
+    // Function to move player using keyboard
     void PlayerMoveKeyboard () {
         float forceX = 0f;
         float vel = Mathf.Abs(myBody.velocity.x);
 
+        // utilize A, D, up, down, left or right arrow keys to -1 or 1 , 0 if not pressed to 
+        // move the player
         float h = Input.GetAxisRaw("Horizontal");
 
+        // move player right
         if(h > 0) {
             if (vel < maxVelocity)
                 forceX = speed;
@@ -40,6 +51,7 @@ public class Player : MonoBehaviour {
 
             anim.SetBool("Walk", true);
 
+            // go to left
         } else if(h < 0) {
 
             if (vel < maxVelocity)
@@ -54,6 +66,7 @@ public class Player : MonoBehaviour {
             anim.SetBool("Walk", false);
         }
 
+        // apply to rigidbody
         myBody.AddForce(new Vector2(forceX, 0));
                 
 
