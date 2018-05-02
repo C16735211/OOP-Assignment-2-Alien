266dce39083e05a420dca405f2f1b198cd6e6a15
Darren Byrne <c16735211@mydit.ie>
2018-05-02 15:47:00 +0100

    update PlayerBounds and PlayerMoveJoystick scripts

--- Assets/Scripts/Player Scripts/PlayerBounds.cs
+++ Assets/Scripts/Player Scripts/PlayerBounds.cs
@@ -14,13 +14,18 @@ public class PlayerBounds : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        // checks if player is outside of the bounds
+        // don't let the player go out of bounds on the left
 	    if(transform.position.x < minX) {
             Vector3 temp = transform.position;
             temp.x = minX;
             transform.position = temp;
         }
 
-        if(transform.position.x > maxX) {
+
+        // checks if player is outside of the bounds
+        // don't let the player go out of bounds on the right
+        if (transform.position.x > maxX) {
             Vector3 temp = transform.position;
             temp.x = maxX;
             transform.position = temp;
@@ -31,6 +36,9 @@ public class PlayerBounds : MonoBehaviour {
 
         Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
+        // set the values of the players out 
+        // of bounds position can tweek if necessary 
+        // e.g. 0.5f, -0.5f;
         maxX = bounds.x;
         minX = -bounds.x;
 
