f8e4a10f3ee1c988d17fba7efe5a2797951cc252
Darren Byrne <c16735211@mydit.ie>
2018-05-02 12:16:54 +0100

    BGScaler script commeneted

--- Assets/Scripts/Background Scripts/BGScaler.cs
+++ Assets/Scripts/Background Scripts/BGScaler.cs
@@ -8,8 +8,11 @@ public class BGScaler : MonoBehaviour {
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
         Vector3 tempScale = transform.localScale;
 
+        // width of sprite
         float width = sr.sprite.bounds.size.x;
 
+        // set up the world width and height of game 
+        // in the camera area relevant to screen
         float worldHeight = Camera.main.orthographicSize * 2f;
         float worldWidth = worldHeight / Screen.height * Screen.width;
 
